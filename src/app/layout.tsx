dc8fbb02feafa8c7d3fb5c696225897ed6b1e7e0
Vinicius Gava de Oliveira <gavadeoliveira@outlook.com>
2026-03-24 18:29:05 -0400

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Preloader from "@/components/Preloader";
+import { Analytics } from "@vercel/analytics/next";
 
 const pally = localFont({
   src: [
@@ -49,6 +50,7 @@ export default function RootLayout({
       <body className="font-sans text-slate-800 bg-slate-50" suppressHydrationWarning>
         <Preloader />
         {children}
+        <Analytics />
       </body>
     </html>
   );
